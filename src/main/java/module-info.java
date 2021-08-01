8b73862d17d8e7096228178c45c25e79daa74259
Rob Spoor <rob@icemanx.nl>
2021-08-01 16:35:23 +0200

    Split off window subpackage to a separate project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -21,12 +21,10 @@
  * @author Rob Spoor
  */
 module com.github.robtimus.os.windows {
-    requires static transitive java.desktop;
     requires com.sun.jna;
     requires com.sun.jna.platform;
 
     exports com.github.robtimus.os.windows;
     exports com.github.robtimus.os.windows.registry;
     exports com.github.robtimus.os.windows.service;
-    exports com.github.robtimus.os.windows.window;
 }
