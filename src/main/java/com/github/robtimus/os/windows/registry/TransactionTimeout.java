79876ff73e52cb62036669936800508776bd52db
Rob Spoor <rob@icemanx.nl>
2025-05-04 17:44:41 +0200

    Refactored transactions to support more transactional states. Made RegistryIT more repeatable

---
+++ src/main/java/com/github/robtimus/os/windows/registry/TransactionTimeout.java
@@ -0,0 +1,29 @@
+/*
+ * TransactionTimeout.java
+ * Copyright 2025 Rob Spoor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.robtimus.os.windows.registry;
+
+import java.time.Duration;
+
+record TransactionTimeout(Duration timeout) implements TransactionOption {
+
+    @Override
+    @SuppressWarnings("nls")
+    public final String toString() {
+        return "timeout=" + timeout;
+    }
+}
